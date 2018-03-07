69c49560e5cab9733c43fc6e6be09f69616b3445
wskeee <361733529@qq.com>
2018-03-07 09:58:38 +0800

    添加百度站点统计

--- frontend/web/js/hm.js
+++ frontend/web/js/hm.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function () {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?457e148a60754007b1717e816f602824";
+    hm.src = "https://hm.baidu.com/hm.js?a60158e9248d31ee3fc2ae97b4795695";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
