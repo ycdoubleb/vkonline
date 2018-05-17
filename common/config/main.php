d0c95c469a450c052d0bce2408fc4157e37dec6f
wskeee <361733529@qq.com>
2018-05-17 19:20:24 +0800

    配置上传组件访问权限

--- common/config/main.php
+++ common/config/main.php
@@ -43,6 +43,7 @@ return [
         'allowActions' => [
             'site/*',
             'debug/*',
+            'webuploader/*',
         // The actions listed here will be allowed to everyone including guests.
         // So, 'admin/*' should not appear here in the production, of course.
         // But in the earlier stages of your development, you may probably want to
