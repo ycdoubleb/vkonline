74ffd750d4864af7692b37e8dd3e4e34d65a48b4
wskeee <361733529@qq.com>
2018-06-12 11:38:47 +0800

    修改视频上传不压缩略图

--- common/modules/webuploader/controllers/DefaultController.php
+++ common/modules/webuploader/controllers/DefaultController.php
@@ -495,7 +495,7 @@ class DefaultController extends Controller {
             //其它文件不创建缩略图，返回''
             return "imgs/upload_filetype_icons/".$this->getExt($fileinfo['extension']).'.png';
         }
-        $thumbpath = $fileinfo['dirname'] . '/thumbs/' . $fileinfo['filename'] . '.jpg';
+        $thumbpath = $fileinfo['dirname'] . '/thumbs/' . $fileinfo['filename'] . '.jpg?rand='. rand(0, 1000);
         $this->mkdir($fileinfo['dirname'] . '/thumbs/');
         Image::$thumbnailBackgroundColor = '000';
         switch ($type) {
