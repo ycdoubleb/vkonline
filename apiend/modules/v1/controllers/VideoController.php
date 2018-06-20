c20cf58484cb64b9179b9208e23a1f1e7d25c47e
wskeee <361733529@qq.com>
2018-06-20 14:57:07 +0800

    修改webuploader使用api接口返回方式

--- apiend/modules/v1/controllers/VideoController.php
+++ apiend/modules/v1/controllers/VideoController.php
@@ -36,33 +36,4 @@ class VideoController extends BaseApiController {
             'create-video' => ['class' => CreateVideoAction::class],
         ]);
     }
-    public function beforeAction($action) {
-        if (parent::beforeAction($action)) {
-            // 5 minutes execution time
-            @set_time_limit(5 * 60);
-            
-            Yii::$app->response->headers->set('Access-Control-Allow-Origin','*');
-            Yii::$app->response->headers->set('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
-            Yii::$app->response->headers->set('Access-Control-Allow-Methods','GET, POST, PUT,DELETE');
-            Yii::$app->response->headers->set('Expires','Mon, 26 Jul 1997 05:00:00 GMT');
-            Yii::$app->response->headers->set('Last-Modified',gmdate("D, d M Y H:i:s") . " GMT");
-            Yii::$app->response->headers->set('Cache-Control','no-store, no-cache, must-revalidate');
-            Yii::$app->response->headers->add('Cache-Control','post-check=0, pre-check=0');
-            Yii::$app->response->headers->set('Pragma','no-cache');
-
-            // Support CORS
-            // header("Access-Control-Allow-Origin: *");
-            // other CORS headers if any...
-            if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-                exit; // finish preflight CORS requests here
-            }
-            if (!empty($_REQUEST['debug'])) {
-                $random = rand(0, intval($_REQUEST['debug']));
-                if ($random === 0) {
-                    //header('HTTP/1.0 500 Internal Server Error');exit;
-                }
-            }
-        };
-        return parent::beforeAction($action);
-    }
 }
