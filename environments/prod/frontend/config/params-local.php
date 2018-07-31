fa4724342dafaa1c347de0b305549579cf26ccf6
wskeee <361733529@qq.com>
2018-07-31 13:45:53 +0800

    修改res配置到公共配置

--- environments/prod/frontend/config/params-local.php
+++ environments/prod/frontend/config/params-local.php
@@ -1,13 +1,4 @@
 <?php
 return [
-    /* 资源库访问 */
-    'res' => [
-        'host' => 'http://res.studying8.com',
-        
-        'auth_action' => '/external/studying8/authentication',      //认证路径
-        'synchronization_user_action' => '/external/studying8/synchronization-user',      //同步用户
-        
-        'library_action' => '/rescenter/at-service/list',           //返回资源库
-        'download_action' => '/rescenter/at-service/download',      //下载路径
-    ],
+    
 ];
