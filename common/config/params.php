185bf906a9d0d4fe0fd0de8d66f9592bb1effaf4
Vito915 <Administrator@Vito>
2018-08-06 15:06:35 +0800

    修改QQ登录的密钥和注释

--- common/config/params.php
+++ common/config/params.php
@@ -35,8 +35,8 @@ return [
     ],
     /* QQAPI配置 */
     'qqLogin' => [
-        "appid" => "101483864",
-        "appkey" => "c11cd3b881df1cb2306259b6dbe5b12c",
+        "appid" => "101489461",
+        "appkey" => "a0ce56f34f2a0ae3ab1d4eb581b52313",
         "callback" => WEB_ROOT."/callback/qq-callback/callback",
         "scope" => "get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr",
         'errorReport' => "true",
