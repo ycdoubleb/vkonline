1962264db05a492e0237e2dbd9f8f285ecbf8b4a
Kiwi <Kiwi@Kiwi-PC>
2018-08-14 17:07:22 +0800

    1、修改“板书工具”是否安装提示

--- frontend/modules/build_course/assets/js/gloudDetect.js
+++ frontend/modules/build_course/assets/js/gloudDetect.js
@@ -5,7 +5,7 @@ $(function () {
                 function () {
                     var bln = confirm("检测到您电脑未安装‘板书工具’ 是否下载安装？");
                     if (bln == true) {
-                        window.location = "https://guanjia.qq.com/sem/198/index.html?ADTAG=media.buy.baidu.SEM";
+                        window.location = "http://file.studying8.com/static/tools/coursemaker/Setup.exe";
                     }
                 });
         event.preventDefault ? event.preventDefault() : event.returnValue = false;
