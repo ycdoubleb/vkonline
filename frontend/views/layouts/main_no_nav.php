8a5609c1f4c9f5bc63cefa4306e1fe950094262a
wskeee <361733529@qq.com>
2018-09-27 19:39:23 +0800

    1、去除百度站点统计

--- frontend/views/layouts/main_no_nav.php
+++ frontend/views/layouts/main_no_nav.php
@@ -22,20 +22,9 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta baidu-gxt-verify-token="ba7c0074cb8ad4749087e8b482ab0afa"><!--百度云观测密钥-->
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
-    <!--百度统计密钥-->
-    <script type="text/javascript">
-        var _hmt = _hmt || [];
-            (function() {
-              var hm = document.createElement("script");
-              hm.src = "https://hm.baidu.com/hm.js?d0f0eac1cb855e1d9ce70e8b2e39b95a";
-              var s = document.getElementsByTagName("script")[0]; 
-              s.parentNode.insertBefore(hm, s);
-        })();
-    </script>
 </head>
 <body>
 <?php $this->beginBody() ?>
