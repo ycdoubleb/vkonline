9f48cd3a02d32f5242358c6ae1c281633d654c80
Vito915 <Administrator@Vito>
2018-11-13 19:29:34 +0800

    第三方QQ登录适配每日一课

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -76,7 +76,7 @@ return [
     'qqLogin' => [
         "appid" => "101500818",
         "appkey" => "edca4d7baa6ef224c99a5580e8059cce",
-        "callback" => WEB_ROOT . "/callback/qq-callback/callback",
+        "callback" => "/callback/qq-callback/callback",
         "scope" => "get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr",
         'errorReport' => "true",
         'storageType' => "file",
--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -47,7 +47,7 @@ return [
     'qqLogin' => [
         "appid" => "101489461",
         "appkey" => "a0ce56f34f2a0ae3ab1d4eb581b52313",
-        "callback" => WEB_ROOT . "/callback/qq-callback/callback",
+        "callback" => "/callback/qq-callback/callback",
         "scope" => "get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr",
         'errorReport' => "true",
         'storageType' => "file",
